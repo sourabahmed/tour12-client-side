e753ef66e5ab41d62de6f357858400f80edee165
unknown <sourabahmed>
2021-10-29 00:27:14 +0600

    header-added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Home from './pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home></Home>
     </div>
   );
 }
